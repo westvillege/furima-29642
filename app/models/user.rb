81516cd7e3f91ca0ab3a5e8aaf30638b049b0a1c
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-03 16:20:33 +0900

    エラーメッセージ設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   has_many :orders
        
   with_options presence: true do
-    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."} 
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"} 
     validates :email, unique: faise, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     validates :encrypted_password, length: { minimum: 7 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :family_name_furi, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_furi, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
+    validates :family_name_furi, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力してください"}
+    validates :first_name_furi, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力してください"}
     validates :birthday, date: true
   end
 end
