947344ccd24210505535007a023e51eb54f98085
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-06 15:19:07 +0900

    新規登録画面実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :family_name, :first_name, :family_name_furi, :first_name_furi, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :family_name, :first_name, :family_name_furi, :first_name_furi, :birthday])
+    devise_parameter_sanitizer.permit(:accout_update, keys: [:name, :family_name, :first_name, :family_name_furi, :first_name_furi, :birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,12 @@ class User < ApplicationRecord
        
   with_options presence: true do
     validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"} 
-    validates :email, unique: faise, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :encrypted_password, length: { minimum: 7 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :email, unique: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, length: { minimum: 7 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
     validates :family_name_furi, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力してください"}
     validates :first_name_furi, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力してください"}
-    validates :birthday, date: true
   end
 end
         
