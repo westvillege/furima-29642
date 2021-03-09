0bcb8f7effffa208cc108a75d01a7155ab8a44f8
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-09 20:04:49 +0900

    指摘事項修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :email, unique: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :password, length: { minimum: 7 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: '全角で入力してください' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: '全角で入力してください' }
     validates :family_name_furi, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）で入力してください' }
