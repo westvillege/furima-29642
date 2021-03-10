e381382542d43ffd23dad1df57b0ac9b93f0188b
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-11 08:40:22 +0900

    Itemsバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   has_one_attached :image
+
+  with_options presence: true do
+    validates :product, length:{maximum:40}
+    validates :description, length:{maximum:1000}
+    validates :price, inclusion: { in: 300..9_999_999 }, format:{ with: /\A[0-9]+\z/ }
+    validates :user
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :condition_id, numericality: { other_than: 1 } 
+    validates :fee_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :time_id, numericality: { other_than: 1 } 
+  end
+
 end
