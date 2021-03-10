c8efe38ed1d6307a7b5bf9df088a0ed525bd5ffc
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-11 08:31:27 +0900

    商品出品テーブル修正

--- db/migrate/20210309141414_create_items.rb
+++ db/migrate/20210309141414_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :product
-      t.text :description
-      t.integer :price
-      t.references :user
-      t.integer :category_id
-      t.integer :condition_id
-      t.integer :fee_id
-      t.integer :prefecture_id
-      t.integer :time_id
+      t.string :product        , null: false
+      t.text :description      , null: false
+      t.integer :price         , null: false
+      t.references :user       , null: false
+      t.integer :category_id   , null: false
+      t.integer :condition_id  , null: false
+      t.integer :fee_id        , null: false
+      t.integer :prefecture_id , null: false
+      t.integer :time_id       , null: false
       t.timestamps
     end
   end
