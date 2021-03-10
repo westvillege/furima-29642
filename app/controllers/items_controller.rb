68db6f28d0966326d79adf2e34a13c8de812b838
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-10 13:13:53 +0900

    ActiveStrageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  private
+  
+  def message_params
+    params.require(:item).permit(:product, :image).merge(user_id: current_user.id)
+  end
 end
