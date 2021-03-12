1714d9d6d786494ff3861dbf9ec6726bafacd5b0
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-12 09:23:43 +0900

    controller(index,create)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @items = Item.all
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -10,7 +10,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end  
   end
 
   private
