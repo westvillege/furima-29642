86b62ce7632b3e13ecc5f593965bee88beed079d
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-29 21:22:58 +0900

    items_controller修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
