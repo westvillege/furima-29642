24a692e06c8c59cc92d160bd114d68b94de92e89
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-29 21:41:26 +0900

    指摘事項修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user_id 
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
