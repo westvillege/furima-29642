a013a9bee4a098dd8ac30e913f6fa56da5e77608
westvillege <73535181+westvillege@users.noreply.github.com>
2021-03-29 19:34:50 +0900

    items_controller修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,11 +32,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
-  
-    def destroy
-      item = Item.find(params[:id])
-      item.destroy
-    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
   end
 
   private
