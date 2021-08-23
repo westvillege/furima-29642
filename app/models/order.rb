2bb450770264f03babd6035d9ee28b6dd3ecf154
westvillege <73535181+westvillege@users.noreply.github.com>
2021-08-23 18:48:50 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
-# class Order < ApplicationRecord
-#  belongs_to :user
-#  belongs_to :item
-# end
+class Order < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+
+  validates :price, presence: true
+  validates :token, presence: true
+end
