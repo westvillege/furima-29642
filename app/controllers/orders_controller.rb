0563601b5df1b20f6e2dc50659be919dff158bc4
westvillege <73535181+westvillege@users.noreply.github.com>
2021-08-23 18:46:46 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,38 @@
-# class OrdersController < ApplicationController
-#  def index
-#    @order = Order.create(order_params)
-#  end
-#
-#  private
-#
-#  def order_params
-#    params.require(:order).permit(:item)
-#  end
-# end
+class OrdersController < ApplicationController
+  
+  def index
+    @order = Order.new(order_params)
+    @item = Item.find(params[:item_id])
+  end
+   
+  def new
+  end
+  
+  def create
+    @item = Item.find(params[:item_id])
+    @order_destination = OrderDestination.new(order_params)   
+     if @order_destination.valid?
+       pay_item
+       @order_destination.save
+       redirect_to root_path
+     else
+       render action: :index
+     end
+  end
+
+  private
+ 
+  def order_params
+   params.permit(:postal_code,:prefecture_id, :city, :ad_detail, :building, :phone, :item_id).merge(token: params[:token], user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_" 
+      Payjp::Charge.create(
+        price: order_params[:id],
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
+  end
+
+end
