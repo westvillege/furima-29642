ca95614d28f0f6d5bd21a1369525f3229d8fd94a
westvillege <73535181+westvillege@users.noreply.github.com>
2021-08-29 12:37:49 +0900

    購入済を購入しようとするとトップページへ実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   def index
     @order_destination = OrderDestination.new
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id
+    if current_user.id == @item.user_id or @item.order.present? then
       redirect_to root_path 
     end
   end
