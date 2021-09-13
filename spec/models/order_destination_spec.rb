c7e7a76fdcef1e4329d7faf9155bdd287c0c887a
westvillege <73535181+westvillege@users.noreply.github.com>
2021-09-13 22:27:25 +0900

    修正

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -3,11 +3,18 @@ require 'rails_helper'
 RSpec.describe OrderDestination, type: :model do
   describe '送付情報の保存' do
     before do
-      @order_destination = FactoryBot.build(:order_destination)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_destination = FactoryBot.build(:order_destination,user_id: user.id,item_id: item.id) 
+      sleep(1)
     end
 
     context '送付情報の保存がうまくいくとき' do
       it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_destination).to be_valid
+      end
+      it 'buildingは入力された場合も保存できること' do
+        @order_destination.building = "ビル"
         expect(@order_destination).to be_valid
       end
     end
